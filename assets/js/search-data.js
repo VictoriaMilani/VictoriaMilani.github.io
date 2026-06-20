bcbc898a9fe45de9f5ead242b2bc07ee63641dd8
VictoriaMilani <75386730+VictoriaMilani@users.noreply.github.com>
2026-06-20 02:28:49 +0000

    Deploying to gh-pages from @ VictoriaMilani/VictoriaMilani.github.io@b0aa9422d93362ef6d97fb72bd76cd6c2281d1a5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,12 +23,26 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
+        },{id: "nav-research",
+          title: "Research",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/research/";
+          },
         },{id: "nav-projects",
           title: "projects",
           description: "A growing collection of your cool projects.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
+          },
+        },{id: "nav-português",
+          title: "Português",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/pt/";
           },
         },{id: "nav-teaching",
           title: "teaching",
